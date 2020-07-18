603d9061af8afb81547746ec3c0af2a9ec526f4a
Logan Campbell <lcampbellmcpherson@zendesk.com>
2020-07-18 15:57:00 +1000

    ported core types

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,48 @@
 use ferris_says::say; // from the previous step
 use std::io::{stdout, BufWriter};
+use std::collections::HashMap;
+
+enum BuiltinFunction {
+    Equals,
+    Plus,
+    Minus,
+    Times,
+    First,
+    Println,
+}
+
+type Environment = HashMap<String, Expression>;
+
+enum Frame {
+    Start(Environment, Expression),
+    AddToEnv(Environment, String),
+    PushBranch(Environment, Expression, Expression),
+    EvalFn(Environment, Vec<Expression>),
+    EvalArgs(Environment, Expression, Vec<Expression>, Vec<Expression>),
+    Stop(Environment, Expression),
+}
+
+type Stack = Vec<Frame>;
+
+enum Expression {
+    True,
+    False,
+    Number(i32),
+    Symbol(String), // Should this be &str instead?
+    List(Vec<Expression>),
+    Function(BuiltinFunction),
+    Lambda(Environment, Vec<String>, Box<Expression>), // Why did I have the ref(env)?
+    Continuation(Stack),
+}
+
+enum ReadResult {
+    EndOfTokens(Vec<Expression>),
+    EndOfExpression(Vec<Expression>, Vec<String>)
+}
+
+// exception ArgumentError(string);
+
+// exception UnbalancedParens;
 
 fn main() {
     let stdout  = stdout();
