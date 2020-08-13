737650376fb3f3928ae2e155a0637330703893e3
Logan Campbell <lcampbellmcpherson@zendesk.com>
2020-08-14 02:34:05 +1000

    can parse nana

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)]
 
-use ferris_says::say; // from the previous step
+use ferris_says::say;
 use std::io::{stdout, BufWriter};
 use std::collections::HashMap;
 use regex::Regex;
@@ -34,7 +34,7 @@ enum Expression {
     True,
     False,
     Number(i32),
-    Symbol(String), // Should this be &str instead?
+    Symbol(String),
     List(Vec<Expression>),
     Function(BuiltinFunction),
     Lambda(Environment, Vec<String>, Box<Expression>), // Why did I have the ref(env)?
@@ -46,15 +46,10 @@ enum ReadResult {
     EndOfExpression(Vec<Expression>, Vec<String>)
 }
 
-// exception ArgumentError(string);
-
-// exception UnbalancedParens;
-
-fn tokenize(code: String) -> Vec<String> {
-    // Should move these out of the function somewhere
+fn tokenize(code: &str) -> Vec<String> {
     let parens = Regex::new(r"(?P<p>[\(\)])").unwrap();
     let whitespace = Regex::new(r"[ \n]+").unwrap();
-    let spacious_code = parens.replace_all(&code, " $p ");
+    let spacious_code = parens.replace_all(code, " $p ");
     let tokens = whitespace
         .split(&spacious_code)
         .filter(|s| !s.is_empty() )
@@ -69,21 +64,49 @@ fn is_number(str: &str) -> bool {
     return digits.is_match(str);
 }
 
-// fn read(expressions: &mut Vec<Expression>, tokens: Vec<&str>) -> ReadResult {
-//     return match tokens[..] {
-//         [] => {
-//             // So rather than mutating expressions should we be cloning and then reversing?
-//             // Or returning a mutable something in EndOfTokens?
-//             expressions.reverse();
-//             return ReadResult::EndOfTokens(expressions.to_vec());
-//         },
-//         ["(", ..] => {
-//             ReadResult::EndOfExpression(expressions.to_vec(), tokens)
-//         }
-//         // This is just to make the types stop complaining, should be deleted
-//         _ => ReadResult::EndOfExpression(expressions.to_vec(), tokens)
-//     };
-// }
+fn read(mut expressions: Vec<Expression>, mut tokens: Vec<String>) -> ReadResult {
+    let head = tokens.pop();
+
+    match head {
+        None => ReadResult::EndOfTokens(expressions),
+        Some(head) => {
+            match &head[..] {
+                "(" => {
+                    match read(vec![], tokens) {
+                        ReadResult::EndOfTokens(_) => panic!("Unbalanced parens"),
+                        ReadResult::EndOfExpression(nested_expressions, new_tokens) => {
+                            expressions.push(Expression::List(nested_expressions));
+                            return read(expressions, new_tokens)
+                        }
+                    }
+                },
+                ")" => return ReadResult::EndOfExpression(expressions, tokens),
+                "true" => expressions.push(Expression::True),
+                "false" => expressions.push(Expression::False),
+                n if is_number(n) => {
+                    let head_as_int: i32 = n.parse().unwrap();
+                    expressions.push(Expression::Number(head_as_int));
+                },
+                _ => expressions.push(Expression::Symbol(head))
+            }
+
+            read(expressions, tokens)
+        }
+    }
+}
+
+fn read_tokens(mut tokens: Vec<String>) -> Vec<Expression> {
+    tokens.reverse();
+
+    match read(vec![], tokens) {
+        ReadResult::EndOfTokens(result) => result,
+        ReadResult::EndOfExpression(_, _) => panic!("Unbalanced parens")
+    }
+}
+
+fn parse(code: &str) -> Vec<Expression> {
+    read_tokens(tokenize(&code))
+}
 
 fn main() {
     let stdout  = stdout();
@@ -94,6 +117,36 @@ fn main() {
     say(message.as_bytes(), width, &mut writer).unwrap();
 
     assert_eq!(BuiltinFunction::Equals, BuiltinFunction::Equals);
-    let greeting = "(hello)".to_string();
-    assert_eq!(vec!["(", "hello", ")"], tokenize(greeting))
+    assert_eq!(vec!["(", "hello", ")"], tokenize(&"(hello)"));
+
+    assert_eq!(vec![Expression::True], parse("true"));
+    assert_eq!(vec![Expression::False], parse("false"));
+    assert_eq!(vec![Expression::Symbol("potato".to_string())], parse("potato"));
+    assert_eq!(vec![Expression::Number(1)], parse("1"));
+    assert_eq!(
+        vec![
+            Expression::List(vec![
+                Expression::Symbol("+".to_string()),
+                Expression::Number(1),
+                Expression::Number(2),
+            ])
+        ],
+        parse("(+ 1 2)")
+    );
+
+    assert_eq!(
+        vec![
+            Expression::List(vec![
+                Expression::Symbol("+".to_string()),
+                Expression::Number(1),
+                Expression::Number(2),
+                Expression::List(vec![
+                    Expression::Symbol("+".to_string()),
+                    Expression::Number(3),
+                    Expression::Number(4),
+                ]),
+            ])
+        ],
+        parse("(+ 1 2 (+ 3 4))")
+    );
 }
