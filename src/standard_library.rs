4bcd1512abebed85aafd17f5f2031a7d5ba08f10
Logan Campbell <colin@logaan.net>
2020-08-22 16:36:52 +1000

    implemented builtin_apply

--- src/standard_library.rs
+++ src/standard_library.rs
@@ -1,11 +1,16 @@
 use crate::core_types::*;
+use BuiltinFunction::*;
 
 pub fn builtin_apply(func: BuiltinFunction, args: Vec<Expression>) -> Expression {
     match (func, &args[..]) {
-        (BuiltinFunction::Equals, [a, b]) => {
-            if a == b {True}
-            else {False}
-        },
+        (Equals, [a, b]) => if a == b {True} else {False} ,
+        (Plus, [Number(a), Number(b)]) => Number(a + b),
+        (Minus, [Number(a), Number(b)]) => Number(a - b),
+        (Times, [Number(a), Number(b)]) => Number(a * b),
+        // Is there a way of avoiding cloning here?
+        (First, [List(v)]) => v.first().expect("Called first on an empty list").clone(),
+        // There's got to be a way to avoid clone here. Implement Copy? Or Move?
+        (Println, [value]) => { println!("{:?}", value); value.clone()},
         _ => panic!("ArgumentError on builtin function")
     }
 }
